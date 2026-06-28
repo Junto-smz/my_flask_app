0dfc0f05006a1e82a772d2d2996da54149b8bc37
junto <juntosmz36@gmail.com>
2026-06-28 18:52:16 +0900

    Display expense list with total

--- app.py
+++ app.py
@@ -9,17 +9,20 @@ def index():
     description = "遠征にかかった費用を記録・管理するアプリです。"
 
     expenses = [
-        "交通費",
-        "宿泊費",
-        "チケット代",
-        "食費"
+        {"name": "交通費", "amount": 12000},
+        {"name": "宿泊費", "amount": 8000},
+        {"name": "チケット代", "amount": 3500},
+        {"name": "食費", "amount": 2500}
     ]
-
+    total_amount = 0
+    for expense in expenses:
+        total_amount += expense["amount"]
     return render_template(
         "index.html",
         app_name=app_name,
         description=description,
-        expenses=expenses
+        expenses=expenses,
+        total_amount = total_amount
     ) 
 
 
