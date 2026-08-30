52cd55e0b7e4c3b9a5c0d605ed7b67c1be1f3f5a
junto <juntosmz36@gmail.com>
2026-08-30 14:56:21 +0900

    feat(expense): 支出日を登録・一覧表示できるようにする

--- init_db.py
+++ init_db.py
@@ -5,7 +5,9 @@ with sqlite3.connect("./database/database.db") as conn:
         CREATE TABLE IF NOT EXISTS expenses(
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            amount INTEGER NOT NULL)
+            amount INTEGER NOT NULL,
+            spent_on TEXT NOT NULL
+            )
             """)
     
     print("データベースを初期化しました")
