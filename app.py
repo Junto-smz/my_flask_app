7e56c8304c33b54d1d784e3e2b5e7b7f633c992b
junto <juntosmz36@gmail.com>
2026-09-02 12:47:06 +0900

    fix(app): 404エラーページを追加する

--- app.py
+++ app.py
@@ -214,6 +214,9 @@ def delete_expense(expense_id):
         )
     return redirect(url_for("index"))
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"),404
 
 if __name__ == "__main__":
     app.run(debug=True)
